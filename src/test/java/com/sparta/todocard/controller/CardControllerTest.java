fba69fe10a174afcb7d2c05bde2d35f5e92e972f
HEEEUN9812 <heeeun9812@gmail.com>
2024-02-22 16:56:40 +0900

    .

---
+++ src/test/java/com/sparta/todocard/controller/CardControllerTest.java
@@ -0,0 +1,32 @@
+package com.sparta.todocard.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CardControllerTest {
+
+    @Test
+    void createCard() {
+    }
+
+    @Test
+    void getCardList() {
+    }
+
+    @Test
+    void getCard() {
+    }
+
+    @Test
+    void updateCard() {
+    }
+
+    @Test
+    void complete() {
+    }
+
+    @Test
+    void deleteCard() {
+    }
+}
