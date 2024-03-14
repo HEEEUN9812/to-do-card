314bc39bddf8f7ee4c9612e20568fe469b2711e4
HEEEUN9812 <heeeun9812@gmail.com>
2024-03-14 11:26:40 +0900

    :sparkles: JPA Repository

--- src/main/java/com/sparta/todocard/repository/TodoRepository.java
+++ src/main/java/com/sparta/todocard/repository/TodoRepository.java
@@ -1,8 +1,11 @@
 package com.sparta.todocard.repository;
 
 import com.sparta.todocard.entity.Todo;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Integer> {
 
+    Optional<Todo> findById(Long id);
 }
