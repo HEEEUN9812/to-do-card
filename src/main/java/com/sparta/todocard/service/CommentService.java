92c9abea9e2c8019e1d4e740d1c241b748f2f574
HEEEUN9812 <heeeun9812@gmail.com>
2024-03-14 11:20:12 +0900

    :recycle: Transactional 추가

--- src/main/java/com/sparta/todocard/service/CommentService.java
+++ src/main/java/com/sparta/todocard/service/CommentService.java
@@ -3,8 +3,8 @@ package com.sparta.todocard.service;
 
 import com.sparta.todocard.dto.CommentRequestDto;
 import com.sparta.todocard.dto.CommentResponseDto;
-import com.sparta.todocard.entity.Todo;
 import com.sparta.todocard.entity.Comment;
+import com.sparta.todocard.entity.Todo;
 import com.sparta.todocard.entity.User;
 import com.sparta.todocard.repository.CommentRepository;
 import jakarta.validation.ValidationException;
@@ -14,12 +14,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
+@Transactional(readOnly = true)
 public class CommentService {
 
     private final CommentRepository commentRepository;
     private final TodoService todoService;
 
-
+    @Transactional
     public CommentResponseDto createComment(Long todoId, User user, CommentRequestDto requestDto) {
         Todo todo = todoService.findTodo(todoId);
         Comment comment = commentRepository.save(new Comment(requestDto, todo, user));
@@ -27,7 +28,8 @@ public class CommentService {
     }
 
     @Transactional
-    public CommentResponseDto updateComment(Long todoId, Long commentId, CommentRequestDto commentRequestDto, User user) {
+    public CommentResponseDto updateComment(Long todoId, Long commentId,
+        CommentRequestDto commentRequestDto, User user) {
         validateUser(user, commentId);
         Comment comment = commentRepository.findByTodoIdAndId(todoId, commentId)
             .orElseThrow(() -> new NullPointerException("해당 댓글은 존재하지 않습니다."));
