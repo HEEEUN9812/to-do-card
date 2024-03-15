d2a662af1a9f8d5b73e627c61462c432eeaaf7bb
HEEEUN9812 <heeeun9812@gmail.com>
2024-03-15 17:35:16 +0900

    :recycle: todoService 통합 테스트

--- src/test/java/com/sparta/todocard/service/TodoServiceImplIntegrationTest.java
+++ src/test/java/com/sparta/todocard/service/TodoServiceImplIntegrationTest.java
@@ -3,9 +3,9 @@ package com.sparta.todocard.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import com.sparta.todocard.dto.CardCommentResponseDto;
-import com.sparta.todocard.dto.CardRequestDto;
-import com.sparta.todocard.dto.CardResponseDto;
+import com.sparta.todocard.dto.TodoCommentResponseDto;
+import com.sparta.todocard.dto.TodoRequestDto;
+import com.sparta.todocard.dto.TodoResponseDto;
 import com.sparta.todocard.entity.User;
 import com.sparta.todocard.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -22,32 +22,32 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @TestInstance(Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class CardServiceIntegrationTest {
+public class TodoServiceImplIntegrationTest {
 
     @Autowired
-    CardService cardService;
+    TodoServiceImpl todoServiceImpl;
     @Autowired
     UserRepository userRepository;
     User user;
 
-    CardResponseDto cardResponseDto = null;
+    TodoResponseDto todoResponseDto = null;
 
     @Test
     @Order(1)
     @DisplayName("todo 카드 신규 생성")
     void test1() {
         //given
-        CardRequestDto cardRequestDto = new CardRequestDto("title", "content");
+        TodoRequestDto todoRequestDto = new TodoRequestDto("title", "content");
         user = userRepository.findById(1L).orElse(null);
 
         //when
-        CardResponseDto cardResponseDto = cardService.createCard(cardRequestDto, user);
+        TodoResponseDto todoResponseDto = todoServiceImpl.createTodo(todoRequestDto, user);
 
         //then
-        assertNotNull(cardResponseDto.getId());
-        assertEquals(cardRequestDto.getTitle(), cardResponseDto.getTitle());
-        assertEquals(cardRequestDto.getContent(), cardResponseDto.getContent());
-        this.cardResponseDto = cardResponseDto;
+        assertNotNull(todoResponseDto.getId());
+        assertEquals(todoRequestDto.getTitle(), todoResponseDto.getTitle());
+        assertEquals(todoRequestDto.getContent(), todoResponseDto.getContent());
+        this.todoResponseDto = todoResponseDto;
     }
 
     @Test
@@ -55,16 +55,16 @@ public class CardServiceIntegrationTest {
     @DisplayName("생성한 todo 카드 내용 변경")
     void test2() {
         //given
-        Long cardId = this.cardResponseDto.getId();
-        CardRequestDto cardRequestDto = new CardRequestDto("title 수정", "content 수정");
+        Long cardId = this.todoResponseDto.getId();
+        TodoRequestDto todoRequestDto = new TodoRequestDto("title 수정", "content 수정");
 
         //when
-        cardResponseDto = cardService.updateCard(cardId, cardRequestDto, user);
+        todoResponseDto = todoServiceImpl.updateCard(cardId, todoRequestDto, user);
 
         //then
-        assertNotNull(cardResponseDto.getId());
-        assertEquals(cardRequestDto.getTitle(), cardResponseDto.getTitle());
-        assertEquals(cardRequestDto.getContent(), cardResponseDto.getContent());
+        assertNotNull(todoResponseDto.getId());
+        assertEquals(todoRequestDto.getTitle(), todoResponseDto.getTitle());
+        assertEquals(todoRequestDto.getContent(), todoResponseDto.getContent());
     }
 
     @Test
@@ -72,15 +72,15 @@ public class CardServiceIntegrationTest {
     @DisplayName("생성한 todo 카드 조회")
     void test3() {
         //given
-        Long cardId = cardResponseDto.getId();
+        Long cardId = todoResponseDto.getId();
 
         //when
-        CardCommentResponseDto cardCommentResponseDto = cardService.getCard(cardId);
+        TodoCommentResponseDto todoCommentResponseDto = todoServiceImpl.getTodo(cardId);
 
         //then
-        assertEquals(this.cardResponseDto.getId(), cardCommentResponseDto.getId());
-        assertEquals(this.cardResponseDto.getTitle(), cardCommentResponseDto.getTitle());
-        assertEquals(this.cardResponseDto.getContent(), cardCommentResponseDto.getContent());
+        assertEquals(this.todoResponseDto.getId(), todoCommentResponseDto.getId());
+        assertEquals(this.todoResponseDto.getTitle(), todoCommentResponseDto.getTitle());
+        assertEquals(this.todoResponseDto.getContent(), todoCommentResponseDto.getContent());
 
     }
 
@@ -89,10 +89,10 @@ public class CardServiceIntegrationTest {
     @DisplayName("생성한 todo 카드 삭제")
     void test4() {
         //given
-        Long cardId = cardResponseDto.getId();
+        Long cardId = todoResponseDto.getId();
 
         //when
-        Long resultId = cardService.deleteCard(cardId, user);
+        Long resultId = todoServiceImpl.deleteCard(cardId, user);
 
         //then
         assertEquals(cardId, resultId);
